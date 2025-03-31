a283db91dc528641547935b43d191bd7456b5323
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2025-03-31 14:57:00 +0300

    fix: рефакторит код promo.js

--- source/js/modules/promo.js
+++ source/js/modules/promo.js
@@ -1,28 +1,27 @@
 const promoButtons = document.querySelectorAll('.promo__content-button');
 const contentWrapper = document.querySelector('[data-id="content-wrapper"]');
 
+const resetDataset = () => {
+  promoButtons.forEach((button) => {
+    if (button.dataset.active === 'is-active') {
+      button.dataset.active = 'not-active';
+      const slide = contentWrapper.querySelector(`[data-id="${button.dataset.id}"]`);
+      slide.dataset.active = 'not-active';
+    }
+  });
+}
+
 const switchItem = () => {
   promoButtons.forEach((button) => {
     button.addEventListener('click', () => {
-      promoButtons.forEach((button) => {
-        if (button.dataset.active === 'is-active') {
-          button.dataset.active = 'not-active';
-          console.log(button);
-          const slide = contentWrapper.querySelector(`[data-id="${button.dataset.id}"]`);
-          slide.dataset.active = 'not-active';
-          }
-      });
+      resetDataset();
 
       button.dataset.active = 'is-active';
       const slide = contentWrapper.querySelector(`[data-id="${button.dataset.id}"]`);
       slide.dataset.active = 'is-active';
-
-      console.log(slide);
-      console.log(button.dataset.id);
     })
   })
 }
-
 
 const initPromoSwitcher = () => {
   switchItem();
