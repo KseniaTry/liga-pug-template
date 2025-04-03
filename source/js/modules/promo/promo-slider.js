94f07f5fed5bcfebc02994c598fd1ab312059d5a
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2025-04-03 22:55:58 +0300

    fix(promo): исправляет карточки, чтобы картинка накладывалась вверху слайда на mobile, позиционирует кнопки переключения слайдов в соответствии с макетом

--- source/js/modules/promo/promo-slider.js
+++ source/js/modules/promo/promo-slider.js
@@ -4,6 +4,8 @@ const promoSlider = document.querySelector('.promo__content');
 const breakpoint = window.matchMedia('(max-width: 767px)');
 const promoSwiperWrapper = document.querySelector('.promo__content-wrapper');
 const promoSlides = document.querySelectorAll('.promo__slide');
+const buttonNext = document.querySelector('.promo__button--next');
+const buttonPrev = document.querySelector('.promo__button--prev');
 const TABLET_MIN_WIDTH = 768;
 let promoSwiper;
 
@@ -18,9 +20,18 @@ const initPromoSwiper = () => {
       prevEl: '.promo__button--prev',
       nextEl: '.promo__button--next',
     },
-    // on: {
-    //   init() {
-    //     var swiper = this;
+    on: {
+      init() {
+        // var swiper = this;
+        // buttonNext.addEventListener('click', () => {
+        //   next();
+        // })
+
+        // buttonPrev.addEventListener('click', () => {
+        //   prev();
+        // })
+      }
+    }
 
     //     if (swiper.originalParams.loop && swiper.loopedSlides < swiper.originalParams.slidesPerView) {
     //     swiper.params.slidesPerView = swiper.loopedSlides;
@@ -28,8 +39,28 @@ const initPromoSwiper = () => {
     //     swiper.init();
     //     }
     // }
-    //   }
-  });
+      // }
+});
+}
+
+
+
+
+
+function next() {
+  if (data[promoSwiper.realIndex + 1]) {
+    promoSwiper.slideNext(500)
+  } else {
+    promoSwiper.slideToLoop(0)
+  }
+}
+
+function prev() {
+  if (data[promoSwiper.realIndex - 1]) {
+    promoSwiper.slidePrev(500)
+  } else {
+    promoSwiper.slideToLoop(data.length - 1)
+  }
 }
 
 const breakpointChecker = () => {
