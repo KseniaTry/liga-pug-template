d2aeeb1dcd1e848720216c076bd81bb7cff2da64
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2025-04-04 11:52:09 +0300

    fix(directions): добавляет реализацию swiper, рефакторит код JS

--- source/js/modules/directions/directions-slider.js
+++ source/js/modules/directions/directions-slider.js
@@ -1,51 +1,36 @@
-// import Swiper, {SwiperOptions} from "swiper"
-// import Swiper from 'swiper'
 import { addSwiperClass, removeSwiperClass } from '../../utils/swiper-class-switcher';
 import { changeCardsColor, resetCardsColor } from './directions-color-change';
-// import { Navigation } from 'swiper/modules/navigation/navigation';
-// import 'swiper/swiper.min.css';
 
 // инициализация свайпера происходит только при переключении на мобильную версию
-const directionsSlider = document.querySelector('.directions__swiper');
+const directionsSlider = document.querySelector('[data-class="directions-swiper"]');
 const breakpoint = window.matchMedia('(max-width: 767px)');
-const directionsSwiperWrapper = document.querySelector('.directions__swiper-wrapper');
-const directionsSwiperContainer = document.querySelector('.directions__swiper');
-const directionsSlides = document.querySelectorAll('.directions__card');
+const directionsSwiperWrapper = document.querySelector('[data-class="directions-swiper-wrapper"]');
+const directionsSlides = document.querySelectorAll('[data-class="directions-slide"]');
 const TABLET_MIN_WIDTH = 768;
 let directionsSwiper;
 
 const initDirectionsSwiper = () => {
   directionsSwiper = new Swiper(directionsSlider, {
     slideClass: 'directions__card',
-    slidesPerView: 1,
+    slidesPerView: 1.2,
+    spaceBetween: 12,
     loop: true,
-    autoHeight: true,
-    allowTouchMove: true,
+    loopedSlides: 1,
     navigation: {
       prevEl: '.directions__button--prev',
       nextEl: '.directions__button--next',
     },
-    on: {
-      init() {
-        var swiper = this;
-        if (swiper.originalParams.loop && swiper.loopedSlides < swiper.originalParams.slidesPerView) {
-        swiper.params.slidesPerView = swiper.loopedSlides;
-        swiper.destroy(false, false);
-        swiper.init();
-        }
-    }
-      }
   });
 }
 
 const breakpointChecker = () => {
   if (breakpoint.matches) {
     changeCardsColor();
-    addSwiperClass(directionsSwiperContainer, directionsSwiperWrapper, directionsSlides);
+    addSwiperClass(directionsSlider, directionsSwiperWrapper, directionsSlides);
     initDirectionsSwiper();
   } else {
     resetCardsColor();
-    removeSwiperClass(directionsSwiperContainer, directionsSwiperWrapper, directionsSlides);
+    removeSwiperClass(directionsSlider, directionsSwiperWrapper, directionsSlides);
     directionsSwiper.destroy();
   }
 };
@@ -55,7 +40,7 @@ const initDirectionsSlider = () => {
 
   if (window.innerWidth < TABLET_MIN_WIDTH) {
     changeCardsColor();
-    addSwiperClass(directionsSwiperContainer, directionsSwiperWrapper, directionsSlides);
+    addSwiperClass(directionsSlider, directionsSwiperWrapper, directionsSlides);
     initDirectionsSwiper();
   }
 };
